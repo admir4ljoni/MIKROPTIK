aa80f9d45d3d146c41f194dea1ee46cd9d9083cc
admir4ljoni <akuncadangannn2020@gmail.com>
2026-08-15 01:37:26 +0700

    config: tambahkan site domain mikroptik.ptik.uns.ac.id pada astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://mikroptik.ptik.uns.ac.id',
   // Atur base path secara otomatis jika berada di lingkungan GitHub Actions (GitHub Pages subpath /MIKROPTIK/)
   base: process.env.GITHUB_ACTIONS ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || 'MIKROPTIK'}/` : '/',
 });
